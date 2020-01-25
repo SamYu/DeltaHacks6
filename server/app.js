47780feb615a5c59b9e91f415d67a3d822db69d4
Sam Yu <smyu03@gmail.com>
2020-01-25 15:13:11 -0500

    remove react

--- server/app.js
+++ server/app.js
@@ -1,21 +1,14 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'localhost:3000');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
-app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/test', (req, res) => {
     res.send({message: 'API is working!'});
 })
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.send({message: "hello world"} );
 });
 
 app.listen(8080, () => console.log('Server started...!'));
