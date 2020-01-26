35ba96ea27d4331815ccd2898c771f34dba25b19
Sam Yu <smyu03@gmail.com>
2020-01-26 18:08:36 -0500

    update hack track with defects

--- src/scripts/trackHand.js
+++ src/scripts/trackHand.js
@@ -12,6 +12,8 @@ cv['onRuntimeInitialized']=()=>{
         // remove noise
           cv.threshold(dest, dest, 200, 255, cv.THRESH_BINARY);
     };
+
+    // Get Contour
     const getHandContour = (src, dst) => {
         const contours = new cv.MatVector()
         const hierarchy = new cv.Mat()
@@ -32,16 +34,57 @@ cv['onRuntimeInitialized']=()=>{
             maxAreaContour = contours.get(i);
           }
       }
-      return maxAreaContour;
+      console.log(contours);
+      const maxContour = new cv.MatVector()
+      maxContour.push_back(maxAreaContour);
+      return {maxContour, hierarchy};
     };
 
-    
+    const getConvexHull = (contours, hierarchy, dst) => {
+      // approximates each contour to convex hull
+      let hull = new cv.MatVector();
+      let indices = new cv.MatVector();
+      
+      for (let i = 0; i < contours.size(); ++i) {
+        let tmpHull = new cv.Mat();
+        let index = new cv.Mat();
+        let defect = new cv.Mat();
+        let cnt = contours.get(i);
+        cv.convexHull(cnt, tmpHull, false, true);
+        cv.convexHull(cnt, index, false, false);
+        cv.convexityDefects(cnt, index, defect);
+        hull.push_back(tmpHull);
+        indices.push_back(index);
+        for (let i = 0; i < defect.rows; ++i) {
+          let start = new cv.Point(cnt.data32S[defect.data32S[i * 4] * 2],
+                                   cnt.data32S[defect.data32S[i * 4] * 2 + 1]);
+          let end = new cv.Point(cnt.data32S[defect.data32S[i * 4 + 1] * 2],
+                                 cnt.data32S[defect.data32S[i * 4 + 1] * 2 + 1]);
+          let far = new cv.Point(cnt.data32S[defect.data32S[i * 4 + 2] * 2],
+                                 cnt.data32S[defect.data32S[i * 4 + 2] * 2 + 1]);
+          cv.line(dst, start, end, new cv.Scalar(255, 0, 0), 2, cv.LINE_AA, 0);
+          cv.circle(dst, far, 3, new cv.Scalar(255, 0, 0, 255), 5);
+        }
+        cnt.delete(); tmpHull.delete(); index.delete(); defect.delete();
+      }
+      // draw contours with random Scalar
+      for (let i = 0; i < contours.size(); ++i) {
+        let colorHull = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255),
+                                      Math.round(Math.random() * 255));
+        cv.drawContours(dst, hull, i, colorHull, 5, 8, hierarchy, 0);
+      }
+      console.log(hull.get(0));
+      console.log(indices.get(0))
+      return {hull, indices};
+    }
+
     let imgElement = document.getElementById('imageSrc');
     let src = cv.imread(imgElement);
 
     let dst = new cv.Mat();
     makeHandMask(src, dst);
-    contours = getHandContour(dst, dst);
+    handContour = getHandContour(dst, dst);
+    convexHull = getConvexHull(handContour.maxContour, handContour.hierarchy, dst);
     cv.imshow('outputCanvas', dst);
     src.delete();
     dst.delete();
